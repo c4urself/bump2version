16f040c403005fb4b042dc45fab0abcd8bb0ff7e
Jeremy Carroll <jjc@syapse.com>
2018-08-24 04:58:53 -0700

    Addressing encoding in test_usage_string_fork

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,24 +2,20 @@
 
 from __future__ import unicode_literals, print_function
 
+import argparse
+import bumpversion
+import mock
 import os
 import pytest
-import sys
-import logging
-import mock
-
-import argparse
+import six
 import subprocess
-from os import curdir, makedirs, chdir, environ
-from os.path import join, curdir, dirname
-from shlex import split as shlex_split
-from textwrap import dedent
-from functools import partial
-
-import bumpversion
 
 from bumpversion import main, DESCRIPTION, WorkingDirectoryIsDirtyException, \
     split_args_in_optional_and_positional
+from functools import partial
+from os import environ
+from shlex import split as shlex_split
+from textwrap import dedent
 
 
 def _get_subprocess_env():
@@ -157,16 +153,17 @@ def test_usage_string(tmpdir, capsys):
 
 def test_usage_string_fork(tmpdir, capsys):
     tmpdir.chdir()
+    kwargs = {} if six.PY2 else {"encoding": "utf-8"}
 
     try:
-        out = check_output('bumpversion --help', shell=True, stderr=subprocess.STDOUT)
+        out = check_output('bumpversion --help', shell=True, stderr=subprocess.STDOUT, **kwargs)
     except subprocess.CalledProcessError as e:
         out = e.output
 
-    if not b'usage: bumpversion [-h]' in out:
+    if not 'usage: bumpversion [-h]' in out:
         print(out)
 
-    assert b'usage: bumpversion [-h]' in out
+    assert 'usage: bumpversion [-h]' in out
 
 
 @pytest.mark.parametrize(("vcs"), [xfail_if_no_git("git"), xfail_if_no_hg("hg")])
