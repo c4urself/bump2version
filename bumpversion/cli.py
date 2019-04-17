6e90254ea749bee15520070f544456d2a046d75f
Floris Lambrechts <florisla@gmail.com>
2019-04-17 21:43:07 +0200

    Refactor: avoid the body-in-if antipattern
    http://wiki.c2.com/?ArrowAntiPattern

--- bumpversion/cli.py
+++ bumpversion/cli.py
@@ -204,7 +204,6 @@ def _determine_config_file(known_args):
     return config_file, explicit_config
 
 
-
 def _load_configuration(config_file, defaults, explicit_config, files, part_configs):
     # setup.cfg supports interpolation - for compatibility we must do the same.
     if os.path.basename(config_file) == "setup.cfg":
@@ -215,120 +214,118 @@ def _load_configuration(config_file, defaults, explicit_config, files, part_conf
     config.optionxform = lambda option: option
     config.add_section("bumpversion")
     config_file_exists = os.path.exists(config_file)
-    if config_file_exists:
 
-        logger.info("Reading config file {}:".format(config_file))
-        # TODO: this is a DEBUG level log
-        logger.info(io.open(config_file, "rt", encoding="utf-8").read())
-
-        try:
-            # TODO: we're reading the config file twice.
-            config.read_file(io.open(config_file, "rt", encoding="utf-8"))
-        except AttributeError:
-            # python 2 standard ConfigParser doesn't have read_file,
-            # only deprecated readfp
-            config.readfp(io.open(config_file, "rt", encoding="utf-8"))
-
-        log_config = StringIO()
-        config.write(log_config)
-
-        if "files" in dict(config.items("bumpversion")):
-            warnings.warn(
-                "'files =' configuration will be deprecated, please use [bumpversion:file:...]",
-                PendingDeprecationWarning,
-            )
-
-        defaults.update(dict(config.items("bumpversion")))
-
-        for listvaluename in ("serialize",):
-            try:
-                value = config.get("bumpversion", listvaluename)
-                defaults[listvaluename] = list(
-                    filter(None, (x.strip() for x in value.splitlines()))
-                )
-            except NoOptionError:
-                pass  # no default value then ;)
-
-        for boolvaluename in ("commit", "tag", "dry_run"):
-            try:
-                defaults[boolvaluename] = config.getboolean(
-                    "bumpversion", boolvaluename
-                )
-            except NoOptionError:
-                pass  # no default value then ;)
-
-        for section_name in config.sections():
-
-            section_name_match = re.compile("^bumpversion:(file|part):(.+)").match(
-                section_name
-            )
-
-            if not section_name_match:
-                continue
-
-            section_prefix, section_value = section_name_match.groups()
-
-            section_config = dict(config.items(section_name))
-
-            if section_prefix == "part":
-
-                ThisVersionPartConfiguration = NumericVersionPartConfiguration
-
-                if "values" in section_config:
-                    section_config["values"] = list(
-                        filter(
-                            None,
-                            (x.strip() for x in section_config["values"].splitlines()),
-                        )
-                    )
-                    ThisVersionPartConfiguration = ConfiguredVersionPartConfiguration
-
-                part_configs[section_value] = ThisVersionPartConfiguration(
-                    **section_config
-                )
-
-            elif section_prefix == "file":
-
-                filename = section_value
-
-                if "serialize" in section_config:
-                    section_config["serialize"] = list(
-                        filter(
-                            None,
-                            (
-                                x.strip().replace("\\n", "\n")
-                                for x in section_config["serialize"].splitlines()
-                            ),
-                        )
-                    )
-
-                section_config["part_configs"] = part_configs
-
-                if "parse" not in section_config:
-                    section_config["parse"] = defaults.get(
-                        "parse", r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
-                    )
-
-                if "serialize" not in section_config:
-                    section_config["serialize"] = defaults.get(
-                        "serialize", [str("{major}.{minor}.{patch}")]
-                    )
-
-                if "search" not in section_config:
-                    section_config["search"] = defaults.get(
-                        "search", "{current_version}"
-                    )
-
-                if "replace" not in section_config:
-                    section_config["replace"] = defaults.get("replace", "{new_version}")
-
-                files.append(ConfiguredFile(filename, VersionConfig(**section_config)))
-
-    else:
+    if not config_file_exists:
         message = "Could not read config file at {}".format(config_file)
         if explicit_config:
             raise argparse.ArgumentTypeError(message)
         logger.info(message)
+        return config, config_file_exists
+
+    logger.info("Reading config file {}:".format(config_file))
+    # TODO: this is a DEBUG level log
+    logger.info(io.open(config_file, "rt", encoding="utf-8").read())
+
+    try:
+        # TODO: we're reading the config file twice.
+        config.read_file(io.open(config_file, "rt", encoding="utf-8"))
+    except AttributeError:
+        # python 2 standard ConfigParser doesn't have read_file,
+        # only deprecated readfp
+        config.readfp(io.open(config_file, "rt", encoding="utf-8"))
+
+    log_config = StringIO()
+    config.write(log_config)
+
+    if "files" in dict(config.items("bumpversion")):
+        warnings.warn(
+            "'files =' configuration will be deprecated, please use [bumpversion:file:...]",
+            PendingDeprecationWarning,
+        )
+
+    defaults.update(dict(config.items("bumpversion")))
+
+    for listvaluename in ("serialize",):
+        try:
+            value = config.get("bumpversion", listvaluename)
+            defaults[listvaluename] = list(
+                filter(None, (x.strip() for x in value.splitlines()))
+            )
+        except NoOptionError:
+            pass  # no default value then ;)
+
+    for boolvaluename in ("commit", "tag", "dry_run"):
+        try:
+            defaults[boolvaluename] = config.getboolean(
+                "bumpversion", boolvaluename
+            )
+        except NoOptionError:
+            pass  # no default value then ;)
+
+    for section_name in config.sections():
+        section_name_match = re.compile("^bumpversion:(file|part):(.+)").match(
+            section_name
+        )
+
+        if not section_name_match:
+            continue
+
+        section_prefix, section_value = section_name_match.groups()
+
+        section_config = dict(config.items(section_name))
+
+        if section_prefix == "part":
+            ThisVersionPartConfiguration = NumericVersionPartConfiguration
+
+            if "values" in section_config:
+                section_config["values"] = list(
+                    filter(
+                        None,
+                        (x.strip() for x in section_config["values"].splitlines()),
+                    )
+                )
+                ThisVersionPartConfiguration = ConfiguredVersionPartConfiguration
+
+            part_configs[section_value] = ThisVersionPartConfiguration(
+                **section_config
+            )
+
+        elif section_prefix == "file":
+            filename = section_value
+
+            if "serialize" in section_config:
+                section_config["serialize"] = list(
+                    filter(
+                        None,
+                        (
+                            x.strip().replace("\\n", "\n")
+                            for x in section_config["serialize"].splitlines()
+                        ),
+                    )
+                )
+
+            section_config["part_configs"] = part_configs
+
+            if "parse" not in section_config:
+                section_config["parse"] = defaults.get(
+                    "parse", r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
+                )
+
+            if "serialize" not in section_config:
+                section_config["serialize"] = defaults.get(
+                    "serialize", [str("{major}.{minor}.{patch}")]
+                )
+
+            if "search" not in section_config:
+                section_config["search"] = defaults.get(
+                    "search", "{current_version}"
+                )
+
+            if "replace" not in section_config:
+                section_config["replace"] = defaults.get("replace", "{new_version}")
+
+            files.append(ConfiguredFile(filename, VersionConfig(**section_config)))
+
     return config, config_file_exists
 
 
