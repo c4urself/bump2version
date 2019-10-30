f7d1e265e72df75634b7b85f5952d07504fa7f4b
kyluca <kyluca3112@gmail.com>
2019-10-30 22:14:39 +1000

    Use the correct single string method for passing args into subprocess calls on both Linux and Windows

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -37,12 +37,12 @@ check_call = partial(subprocess.check_call, env=SUBPROCESS_ENV)
 check_output = partial(subprocess.check_output,  env=SUBPROCESS_ENV)
 
 xfail_if_no_git = pytest.mark.xfail(
-  call(["git version"]) != 0,
+  call("git version") != 0,
   reason="git is not installed"
 )
 
 xfail_if_no_hg = pytest.mark.xfail(
-  call(["hg version"]) != 0,
+  call("hg version") != 0,
   reason="hg is not installed"
 )
 
